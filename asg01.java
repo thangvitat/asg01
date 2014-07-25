793c988178446e818e46b1fae80e33d156505bee
thangvitat <tatthang1106@gmail.com>
2014-07-25 10:36:57 +0700

    e nop lai asg01 file truoc e de ten la -asg01 thay ko nhan dc mong thay thong cam

---
+++ asg01.java
@@ -0,0 +1,11 @@
+
+public class asg01 {
+	public static void main(String [] args){
+		System.out.println("ViTatThang\t11020304\tK56CC\tthangvitat\tatthang1106@gmail.com");
+		for(int i=9;i>=1;i--){
+			System.out.println(i+" bottles of beer on the wall, "+i+"bottles of beer.\n	Take one down, pass it around,\n");
+		}
+		System.out.println("No more bottles of beer on the wall.");
+	}
+
+}
